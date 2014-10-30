f9ae6d72587462a34a2f277713c30bb32f08bc4e
Cesar <cesar6134@gmail.com>
2014-10-30 15:24:54 +0800

    Modified the express 3 to 4

--- server.js
+++ server.js
@@ -1,18 +1,22 @@
 var express = require('express');
-
+var router = express.Router();
 var app = module.exports = express();
 
-app.configure(function(){
-  // Here we require the prerender middleware that will handle requests from Search Engine crawlers
-  // We set the token only if we're using the Prerender.io service
-  app.use(require('prerender-node').set('prerenderToken', 'YOUR API TOKEN HERE!'));
-  app.use(express.static("public"));
-  app.use(app.router);
-});
+// app.configure(function(){
+//   // Here we require the prerender middleware that will handle requests from Search Engine crawlers
+//   // We set the token only if we're using the Prerender.io service
+//   app.use(require('prerender-node').set('prerenderToken', 'YOUR API TOKEN HERE!'));
+//   app.use(express.static("public"));
+//   app.use(app.router);
+// });
+
+app.use(require('prerender-node').set('prerenderToken', 'MqMxhIyu20yV9Yvypb0L'));
+app.use(express.static("public"));
+app.use(router);
 
 // This will ensure that all routing is handed over to AngularJS
 app.get('*', function(req, res){
-	res.sendfile('./public/index.html');
+	res.sendFile('public/index.html', {root: __dirname});
 });
 
 app.listen(8081);
